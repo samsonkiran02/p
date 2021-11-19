d7617fa877dcfe69090b0a43f0474fa0122409b8
Samson kiran <82866972+samsonkiran02@users.noreply.github.com>
2021-11-19 12:50:03 +0530

    Create GA-id-001.js

---
+++ script/js/Google_Analytics/GA-id-001.js
@@ -0,0 +1,9 @@
+document.write( '<!-- Global site tag (gtag.js) - Google Analytics -->\n' );
+document.write( '	<script async src=\"https://www.googletagmanager.com/gtag/js?id=G-HQKED2YTVH\"></script>\n' );
+document.write( '	<script>\n' );
+document.write( '		window.dataLayer = window.dataLayer || [];\n' );
+document.write( '		  function gtag(){dataLayer.push(arguments);}\n' );
+document.write( '		  gtag(\'js\', new Date());\n' );
+document.write( '		\n' );
+document.write( '		  gtag(\'config\', \'G-HQKED2YTVH\');\n' );
+document.write( '	</script>' );
