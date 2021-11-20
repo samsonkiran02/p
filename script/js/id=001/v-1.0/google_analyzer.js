06e4cca03213fc0f9df8bb882c3a3e95c484e89e
Samson kiran <82866972+samsonkiran02@users.noreply.github.com>
2021-11-20 18:35:15 +0530

    Update google_analyzer.js

--- script/js/id=001/v-1.0/google_analyzer.js
+++ script/js/id=001/v-1.0/google_analyzer.js
@@ -1,3 +1,5 @@
+/* Edit: https://github.com/samsonkiran02/p/blob/main/script/js/id=001/v-1.0/google_analyzer.js */
+
 document.write( '<!-- Global site tag (gtag.js) - Google Analytics -->\n' );
 document.write( '	<script async src=\"https://www.googletagmanager.com/gtag/js?id=G-HQKED2YTVH\"></script>\n' );
 document.write( '	<script>\n' );
